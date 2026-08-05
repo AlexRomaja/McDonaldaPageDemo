2d0b06d295f3e11e46606fc76d04928297474593
AlexRomaja <alex.tover@gmail.com>
2026-08-05 12:48:21 -0600

    [UPDATE] setting heroui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
-import Image from "next/image";
+import { Button } from '@heroui/react';
 
 export default function Home() {
   return (
+    <>
     <h1 className="text-3xl font-bold text-[#2798F5]">
       hello
     </h1>
+    <Button>
+      My world
+    </Button>
+    </>
   );
 }
