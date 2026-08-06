6cc413a17de44e0d120d777d82e72d035bfdbd04
AlexRomaja <alex.tover@gmail.com>
2026-08-06 17:23:01 -0600

    [UPDATE] se agrego botones y texto por encima del header

--- src/Components/organisms/header.tsx
+++ src/Components/organisms/header.tsx
@@ -1,24 +1,31 @@
 import Image from 'next/image';
+import { Button } from '@heroui/react';
 
 
 export default function Header () {
 
+    const buttonClass = 'h-[40px] w-[150px] bg-white text-black font-bold rounded-lg transition-transform duration-200 hover:scale-105 active:scale-95 gap-3';
+
     return(
         <>
-        <div className=" relative ring ring-red-500 h-[545px] w-full overflow-hidden">
+        <div className=" relative xsm:h-[320px] md:h-[545px] w-full overflow-hidden ">
+            {/* Shader */}
+            <div className="absolute inset-0 w-full bg-black/50 md:hidden pointer-events-none z-10" />
+            <div className="absolute inset-y-0 left-0 w-[95%] hidden md:block bg-gradient-to-r from-black/70 via-black/60 to-transparent pointer-events-none z-10" />
+
             {/* Mobil */}
-            <div className='relative h-full w-full block md:hidden z-1'>
+            <div className='absolute inset-0 block md:hidden z-0'>
                 <Image
                 src="https://mcd-landings-l-statics.appmcdonalds.com/uploads-live/mobile_Home_Background_2c12ef6e55.webp"
                 alt="Header background"
                 fill
-                className="fill object-center"
+                className="object-cover object-center"
                 priority
                 />
             </div>
 
              {/* Desk */}
-            <div className='relative h-full w-full md:block hidden'>
+            <div className='absolute inset-0 md:block hidden z-0'>
                 <Image
                 src="https://mcd-landings-l-statics.appmcdonalds.com/uploads-live/home_Background_785a456a07.webp"
                 alt="Header background"
@@ -26,6 +33,26 @@ export default function Header () {
                 className="fill object-center"
                 priority
                 />
+            </div>
+
+            {/* Text Buttons */}
+            <div className='relative h-full w-full pl-0 md:pl-20 z-20 flex flex-col justify-center
+             items-center md:items-start border border-red-500'>
+                <h1 className='text-white font-bold text-[25px] md:text-[40px]'>
+                    Cómo quieres hoy tu pedido
+                </h1>
+
+                <div className='flex flex-row gap-2 pt-3'>
+                    <Button className={`${buttonClass}`} >
+                        <p>🏠</p>
+                        <p>Pickup</p>
+                    </Button>
+                    <Button className={`${buttonClass}`}>
+                        <p>🛩️</p>
+                        <p>McDelivery</p>  
+                    </Button>
+                </div>
+                
             </div>
         </div>
         </>
