ff03d2682e5b9ab10561f6d2bfb69293318108c2
AlexRomaja <alex.tover@gmail.com>
2026-08-11 16:12:20 -0600

    [UPDATE] se agrego banner y carrusel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,15 @@
-import { Button } from '@heroui/react';
+
 import Header from '@/Components/organisms/header';
 import SwipeMenu from '@/Components/organisms/swipeMenu';
 import BannerBigBurger from '@/Components/organisms/bannerBigBurguer';
 import CarruselHome from '@/Components/organisms/carruselHome';
 import CardsHome from '@/Components/organisms/cardsGridHome';
 import CarruselWMovement from '@/Components/organisms/carruselWMove';
+import BannerAmarilloHome from '@/Components/organisms/bannerAmarilloHome';
 
 export default function Home() {
   
   return (
-    <>
     <div className='h-auto'>
       <Header />
       <SwipeMenu />
@@ -17,7 +17,7 @@ export default function Home() {
       <CarruselHome />
       <CardsHome />
       <CarruselWMovement />
+      <BannerAmarilloHome />
     </div>
-    </>
   );
 }
